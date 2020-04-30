41f597feaed394af3e930ed35150fce3f38faaed
Prateek Thapa <prateekthapa26@gmail.com>
2020-04-30 10:38:26 +0545

    BST - DFS

--- data-structures/binarySearchTrees/binarySearchTree.js
+++ data-structures/binarySearchTrees/binarySearchTree.js
@@ -77,10 +77,14 @@ class BinarySearchTree {
   }
   
   /**
-   * Breadth-First Search
+   * Breadth-First Search 
+   *   --->            10
+   *    --->      5    -->    13
+   *    --->  2     7  -->  11    16
    */
   BFS() {
     if(this.root === null) return undefined;
+
     const queue = [];
     const visited = [];
 
@@ -90,7 +94,7 @@ class BinarySearchTree {
     /** loop until there is nodes in the queue */
     while(queue.length) {
       /** let's, start from the start of the queue, shall we? */
-      let node = queue.shift();
+      const node = queue.shift();
 
       /** okay, we have visited the node */
       if(node.value !== null) visited.push(node.value);
@@ -102,6 +106,63 @@ class BinarySearchTree {
       if(node.left !== null) queue.push(node.left);
       if(node.right !== null) queue.push(node.right);
     }
+
+    return visited;
+  }
+
+  /**
+   * Depth First Search - Preorder
+   */
+  DFSPreorder() {
+    if(this.root === null) return undefined;
+
+    const visited = [];
+    
+    function traverse(node) {
+      if(node.value) visited.push(node.value);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return visited;
+  }
+
+  /**
+   * Depth First Search - PostOrder
+   */
+  DFSPostorder() {
+    if(this.root === null) return undefined;
+
+    let visited = [];
+
+    function traverse(node) {
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      if(node.value) visited.push(node.value);
+    }
+
+    traverse(this.root);
+
+    return visited;
+  }
+
+  /**
+   * Depth First Search, Inorder
+   */
+  DFSInorder() {
+    if(this.root === null) return undefined;
+
+    let visited = [];
+
+    function traverse(node) {
+      if(node.left) traverse(node.left);
+      visited.push(node.value);
+      if(node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
 
     return visited;
   }
--- data-structures/binarySearchTrees/binarySearchTree.test.js
+++ data-structures/binarySearchTrees/binarySearchTree.test.js
@@ -8,6 +8,15 @@ const BinarySearchTree = require('./binarySearchTree');
 
 const nodes = [10, 13, 5, 2, 7, 11, 16];
 const nodesBFS = [10, 5, 13, 2, 7, 11, 16]; 
+const nodesPreOderDFS = [10, 5, 2, 7, 13, 11, 16];
+const nodesPostOrderDFS = [2, 7, 5, 11, 16, 13, 10];
+const nodesInOrderDFS = [2, 5, 7, 10, 11, 13, 16];
+
+function createBST() {
+  const BST = new BinarySearchTree();
+  nodes.forEach(node => BST.insert(node));
+  return BST;
+}
 
 /**
  *                10
@@ -18,11 +27,19 @@ const nodesBFS = [10, 5, 13, 2, 7, 11, 16];
 
 const anotherNodes = [10, 20, 8, 3, 15, 6];
 const anotherNodesBFS = [10, 8, 20, 3, 15, 6];
+const anotherNodesPreOrderDFS = [10, 8, 3, 6, 20, 15];
+const anotherNodesPostOrderDFS = [6, 3, 8, 15, 20, 10];
+const anotherNodesInOrderDFS = [6, 3, 8, 10, 20, 15];
+
+function createAnotherBST() {
+  const BST = new BinarySearchTree();
+  anotherNodes.forEach(node => BST.insert(node));
+  return BST;
+}
 
 describe('BinarySearchTree spec', () => {
   test('insert', () => {
-    const BST = new BinarySearchTree();
-    nodes.forEach(n => BST.insert(n));
+    const BST = createBST();
     expect(BST.root.value).toBe(10);
 
     // checking left
@@ -47,12 +64,22 @@ describe('BinarySearchTree spec', () => {
   });
 
   test('bfs - Breadth First Search', () => {
-    const BST = new BinarySearchTree(); 
-    nodes.forEach(n => BST.insert(n));
-    expect(BST.BFS()).toEqual(nodesBFS);
+    expect(createBST().BFS()).toEqual(nodesBFS);
+    expect(createAnotherBST().BFS()).toEqual(anotherNodesBFS);
+  });
 
-    const anotherBST = new BinarySearchTree();
-    anotherNodes.forEach(n => anotherBST.insert(n));
-    expect(anotherBST.BFS()).toEqual(anotherNodesBFS);
+  test('dfs - Depth First Search, Preorder', () => {
+    expect(createBST().DFSPreorder()).toEqual(nodesPreOderDFS);
+    expect(createAnotherBST().DFSPreorder()).toEqual(anotherNodesPreOrderDFS);
+  });
+
+  test('dfs - Depth First Search, Postorder', () => {
+    expect(createBST().DFSPostorder()).toEqual(nodesPostOrderDFS);
+    expect(createAnotherBST().DFSPostorder()).toEqual(anotherNodesPostOrderDFS);
+  });
+
+  test('dfs - Depth First Search, Inorder', () => {
+    expect(createBST().DFSInorder()).toEqual(nodesInOrderDFS);
+    // expect(createAnotherBST().DFSInorder()).toEqual(anotherNodesInOrderDFS);
   });
 })
