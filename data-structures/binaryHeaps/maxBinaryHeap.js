2d038b726b186f28117600262943db8ec2f9f5bf
Prateek Thapa <prateekthapa26@gmail.com>
2020-05-09 00:09:26 +0545

    heapifyDown into another method in maxBinaryHeap

--- data-structures/binaryHeaps/maxBinaryHeap.js
+++ data-structures/binaryHeaps/maxBinaryHeap.js
@@ -36,13 +36,22 @@ class MaxBinaryHeap {
     return this.values[this.getRightChildIdx(parent)];
   }
 
+  hasChild(parent) {
+    return this.hasLeftChild(parent);
+  }
+
+  swap(a, b) {
+    let temp = this.values[a]
+    this.values[a] = this.values[b];
+    this.values[b] = temp;
+  }
+
   /** 
    * insert into the max binary heap
    */
   insert(child) {
     this.values.push(child);
 
-    // FIXME: use method to be more declarative
     let childIdx = this.values.length - 1;
     let parentIdx = this.getParentIdx(childIdx);
     let parent = this.values[parentIdx];
@@ -52,8 +61,7 @@ class MaxBinaryHeap {
      * if child is greater than parent, swap child with parent.
      */
     while (parentIdx >= 0 && child > parent) {
-      this.values[parentIdx] = child;
-      this.values[childIdx] = parent;
+      this.swap(parentIdx, childIdx);
 
       childIdx = parentIdx;
       parentIdx = this.getParentIdx(childIdx);
@@ -69,43 +77,69 @@ class MaxBinaryHeap {
   extractMax() {
     if (this.values.length === 0) return undefined;
     if (this.values.length === 1) return this.values.pop();
-    const rootIdx = 0;
 
     /** Replace the root of the heap with the last element on the last level. */
-    const max = this.values[rootIdx];
+    const max = this.values[0];
 
     /** the last element of the heap becomes the first */
-    this.values[rootIdx] = this.values.pop();
+    this.values[0] = this.values.pop();
 
-    let parentIdx = rootIdx;
-    let parent = this.values[rootIdx];
+    this.heapifyDown();
+
+    return max;
+  }
+
+  /**
+   *     HEAPIFY-DOWN - moves the node at i down the max-heap until it no longer violates the max-heap property
+   * 1. we start from the root of the heap, i.e index : 0
+   * 2. we check the child indexes whether any value of them is greater than parent of not
+   * 3. if greater, swap the parent value with child
+   * 4. carry on the procedure untill it no longer violates the max-heap property
+  */
+  heapifyDown() {
+    let parentIdx = 0;
+    let parent = this.values[0];
 
     while (true) {
-      let nextChildIdx = null;
+      /* keep track of the next child idx to be swapped */
+      let swapChildIdx = null;
 
-      if (!this.hasLeftChild(parentIdx)) break;
+      /** if there is no children, then the procedure is complete */
+      if (!this.hasChild(parentIdx)) break;
 
       let leftChild = this.getLeftChild(parentIdx);
-      if (leftChild > parent) nextChildIdx = this.getLeftChildIdx(parentIdx)
+
+      if (leftChild > parent) {
+        swapChildIdx = this.getLeftChildIdx(parentIdx);
+      }
 
       if (this.hasRightChild(parentIdx)) {
         let rightChild = this.getRightChild(parentIdx);
+        /* remember, while checking for right child, there are 2 possibilities */
         if (
-          (nextChildIdx === null && rightChild > parent) ||
-          (nextChildIdx !== null && rightChild > leftChild)
+          /**
+           * 1. if the right child is greater than the parent. leftChild is lesser than the parent.
+           */
+          (swapChildIdx === null && rightChild > parent) ||
+          /**
+           * 2. Also, we can have a case, where both the right and left child can be greater than the parent.
+           *    In this case, we must use the greatest between the two children to be swapChild
+           */
+          (swapChildIdx !== null && rightChild > leftChild)
         ) {
-          nextChildIdx = this.getRightChildIdx(parentIdx);
+          swapChildIdx = this.getRightChildIdx(parentIdx);
         }
       }
 
-      if (nextChildIdx === null) break;
+      /** the heap satisfies its property, since there is no swap child */
+      if (swapChildIdx === null) break;
 
-      this.values[parentIdx] = this.values[nextChildIdx];
-      this.values[nextChildIdx] = parent;
-      parentIdx = nextChildIdx;
+      /** swap the parent and child */
+      this.swap(parentIdx, swapChildIdx);
+
+      /** update the parentIdx to the new child index */
+      parentIdx = swapChildIdx;
     }
-
-    return max;
   }
 }
 
