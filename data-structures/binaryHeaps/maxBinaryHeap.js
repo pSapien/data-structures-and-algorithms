3acf309e7adc7007251396739c37bbf94cfa8d55
Prateek Thapa <prateekthapa26@gmail.com>
2020-05-08 21:39:42 +0545

    basic extract max from maxBinaryHeap

--- data-structures/binaryHeaps/maxBinaryHeap.js
+++ data-structures/binaryHeaps/maxBinaryHeap.js
@@ -8,8 +8,24 @@ class MaxBinaryHeap {
   /**
    * get the parent index of the any element
    */
-  getParentIdx(idx) {
-    return Math.floor((idx - 1) / 2); 
+  getParentIdx(child) {
+    return Math.floor((child - 1) / 2); 
+  }
+
+  /**
+   * get left child idx of a parent
+   */
+  getLeftChild(parent) {
+    const idx = 2 * parent + 1;
+    return [idx, this.values[idx]];
+  }
+
+  /**
+   * get right child idx of a parent
+   */
+  getRightChild(parent) {
+    const idx = 2 * parent + 2;
+    return [idx, this.values[idx]];
   }
 
   /** 
@@ -36,6 +52,47 @@ class MaxBinaryHeap {
     }
 
     return this.values;
+  }
+
+  /**
+   * extract the max from the heap and reordering the properties in it.
+   */
+  extractMax() {
+    if(this.values.length === 0) return undefined;
+
+    /** Replace the root of the heap with the last element on the last level. */
+    const max = this.values[0];
+
+    /** the last element of the heap becomes the first */
+    this.values[0] = this.values[this.values.length - 1];
+    this.values.length--;
+
+    /** start from the parent idx */
+    let parentIdx = 0;
+    let parent = this.values[parentIdx];
+    let [rightChildIdx, rightChildValue] = this.getRightChild(parentIdx);
+    let [leftChildIdx, leftChildValue] = this.getLeftChild(parentIdx);
+
+    /**
+     * this process is typically called bubble-down
+     * where we check bubble-down to the heap
+    */
+    while(parent > leftChildValue || parent > rightChildValue) {
+      if(leftChildValue > parent) {
+        this.values[parentIdx] = leftChildValue;
+        this.values[leftChildIdx] = parent;
+        parentIdx = leftChildIdx;
+      } else if(rightChildValue > parent) {
+        this.values[parentIdx] = rightChildValue;
+        this.values[rightChildIdx] = parent;
+        parentIdx = rightChildIdx;
+      }
+
+      [rightChildIdx, rightChildValue] = this.getRightChild(parentIdx);
+      [leftChildIdx, leftChildValue] = this.getLeftChild(parentIdx);
+    }
+
+    return max;
   }
 }
 
--- data-structures/binaryHeaps/maxBinaryHeap.test.js
+++ data-structures/binaryHeaps/maxBinaryHeap.test.js
@@ -1,16 +1,30 @@
 const MaxBinaryHeap = require('./maxBinaryHeap');
 
+const HeapWithValues = [41, 39, 33, 18, 21, 12];
+function createHeapWithDefinedValues() {
+  const Heap = new MaxBinaryHeap();
+
+  Heap.insert(41);
+  Heap.insert(39);
+  Heap.insert(33);
+  Heap.insert(18);
+  Heap.insert(27);
+  Heap.insert(12);
+
+  return Heap;
+}
+
 describe('MaxBinaryHeap Spec', () => {
   test('insert', () => {
-    const Heap = new MaxBinaryHeap();
-    Heap.insert(41);
-    Heap.insert(39);
-    Heap.insert(33);
-    Heap.insert(18);
-    Heap.insert(27);
-    Heap.insert(12);
-
+    const Heap = createHeapWithDefinedValues();
     expect(Heap.insert(55)).toEqual([55, 39, 41, 18, 27, 12, 33]);
     expect(Heap.insert(1)).toEqual([55, 39, 41, 18, 27, 12, 33, 1]);
+  });
+
+  test('extractMax', () => {
+    const Heap = createHeapWithDefinedValues();
+    expect(Heap.extractMax()).toBe(41);
+    console.log(Heap.values);
+    // expect(Heap.values).toEqual([39, 27, 33, 18, 12]);
   });
 })
